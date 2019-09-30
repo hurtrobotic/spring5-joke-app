5b3385f4f9ed52aca0f381a4fa5eb427bf1929ce
master <hurtrobotic@protonmail.com>
2019-10-01 00:14:20 +0200

    Init

---
+++ src/main/java/eu/hurtrobotic/joke/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package eu.hurtrobotic.joke.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import eu.hurtrobotic.joke.services.JokeService;
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke",jokeService.getJoke());
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/eu/hurtrobotic/joke/services/JokeService.java
@@ -0,0 +1,6 @@
+package eu.hurtrobotic.joke.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
